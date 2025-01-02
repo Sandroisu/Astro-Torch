327b797802bd531a0146de1fd7afaa63ba28442b
Alex Slatinin <sandroisu1411@gmail.com>
2025-01-02 09:15:43 +0300

    Initial Commit

---
+++ Astro Torch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Astro Torch
+//
+//  Created by Александр Слатинин on 02/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
